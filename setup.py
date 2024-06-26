0f447b9bd8287b002422f30e807e36a4101f367a
JBuffa10 <50505391+JBuffa10@users.noreply.github.com>
2024-06-26 13:10:34 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,20 +18,15 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 # Fields marked as "Optional" may be commented out.
 
 setup(
-    name="change_detection",  # required
+    name="change_detection",
     version="1.0.0",  # required
     author="Jacob Buffa", # optional
-    author_email="jbuffa@astros.com", #optional
+    author_email="jacobbuffa10@gmail.com",
     maintainer = "Jacob Buffa", #optional
-    maintainer_email="jbuffa@astros.com", #optional
-    description="Package used to identify skill changes",  # optional
+    maintainer_email="jacobbuffa10@gmail.com",
+    description="Package used to identify changes in time series data",
     packages=['change_detection'],
     long_description=long_description,  # optional
     long_description_content_type="text/markdown",  # 0ptional (see note above)
-    #url="https://bitbucket.org/Jacob-Buffa/astro_kinematics",  # Optional
     install_requires = ["numpy", "jax", "numpyro"]
-    # project_urls={  # Optional
-    #     "Bug Reports": "https://bitbucket.org/houstonastros/astro_kinematics/issues",
-    #     "Source": "https://bitbucket.org/houstonastros/astro_kinematics/",
-    # },
 )
